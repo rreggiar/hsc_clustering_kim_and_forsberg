3c538c6a61be152f5adb5625a4ddea271bfe8c90
rregiar <rreggiar@ucsc.edu>
2022-01-13 11:10:54 -0800

    delete old data

--- data/old.data/cqn.R
+++
@@ -1,9 +0,0 @@
-library(cqn)
-library(tidyverse)
-
-peaklist <- read.delim(file = "/public/groups/forsberglab/atac_analysis/200519_bamscale/final_peaks_0622.txt")
-counttable <- read.delim(file = "/public/groups/forsberglab/atac_analysis/200519_bamscale/allcounts_0622.txt", row.names = 1)
-
-atac_cqn_peaklist0622 <- cqn(counttable, peaklist$bias, peaklist$width, sizeFactors = NULL, lengthMethod = "fixed")
-
-save(atac_cqn_peaklist0622, file = "/public/groups/forsberglab/atac_analysis/200519_bamscale/cqn_peaklist_0622.Rdata")
